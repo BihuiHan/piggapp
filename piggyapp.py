d9b11173e21f2c60ffb1b778bbca0193ba306531
BihuiHan <hanb0004@e.ntu.edu.sg>
2015-10-05 22:02:17 +0800

    Build python project for piggyapp

---
+++ piggyapp.py
@@ -0,0 +1,29 @@
+
+#STAGE 1: APK to DEX
+#Androguard
+
+
+#STAGE 2: DEX to PDG
+#networkX
+
+
+
+#STAGE 3: Primary and Non-primary Classification
+#step1: feature vectorization into metric space
+
+
+
+#STAGE 4: Piggapp Clustering
+#VPT for NNS
+
+
+
+#STAGE 5: Graph plotting
+
+
+
+#STAGE 6: Accuracy
+
+
+
+
